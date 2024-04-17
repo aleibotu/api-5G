73f4b47443972b6357e5790c06a13aa5a0626113
aleivc <aleivc@outlook.com>
2024-04-17 10:58:03 +0800

    change port for node.

--- index.js
+++ index.js
@@ -122,7 +122,7 @@ async function getData (topic, startTime, endTime) {
 }
 
 
-const port = 3001;
+const port = 3002;
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
